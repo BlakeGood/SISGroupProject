a80764b10a74df04c6618371eaafbdb8022ed924
blake.good <blake.good@MU-C1-08.mullen.pvt.k12.co.us>
2019-02-08 12:41:30 -0700

    We're almost done boys

--- SIS Group Project/src/SwitchClasses.java
+++ SIS Group Project/src/SwitchClasses.java
@@ -11,6 +11,10 @@ public class SwitchClasses
 				Roster.roster();
 				int userChoice = userInput.nextInt();
 				
+				String firstClass = DataBase.students.get(userChoice - 1).getOne();
+				String secondClass = DataBase.students.get(userChoice - 1).getTwo();
+				String thirdClass = DataBase.students.get(userChoice - 1).getThree();
+				
 				System.out.println("Which class would you like to switch " + DataBase.students.get(userChoice - 1).getFirstName() + " " + DataBase.students.get(userChoice - 1).getLastName() + " from?");
 				System.out.println("(1) " + DataBase.students.get(userChoice - 1).getOne());
 				System.out.println("(2) " + DataBase.students.get(userChoice - 1).getTwo());
@@ -24,6 +28,48 @@ public class SwitchClasses
 				System.out.println("(3) " + DataBase.students.get(userChoice - 1).getThree());
 				Scanner userInput3 = new Scanner(System.in);
 				int userClassInto = userInput3.nextInt();
+				
+				if(userClassFrom == 1 && userClassInto == 2)
+					{
+						DataBase.students.get(userChoice - 1).setOne(secondClass);
+						DataBase.students.get(userChoice - 1).setTwo(firstClass);
+					}
+				else if(userClassFrom == 1 && userClassInto == 3)
+					{
+						DataBase.students.get(userChoice - 1).setOne(thirdClass);
+						DataBase.students.get(userChoice - 1).setThree(firstClass);
+					}
+				else if(userClassFrom == 2 && userClassInto == 1)
+					{
+						DataBase.students.get(userChoice - 1).setTwo(firstClass);
+						DataBase.students.get(userChoice - 1).setOne(secondClass);
+					}
+				else if(userClassFrom == 2 && userClassInto == 3)
+					{
+						DataBase.students.get(userChoice - 1).setTwo(thirdClass);
+						DataBase.students.get(userChoice - 1).setThree(secondClass);
+					}
+				else if(userClassFrom == 3 && userClassInto == 1)
+					{
+						DataBase.students.get(userChoice - 1).setThree(firstClass);
+						DataBase.students.get(userChoice - 1).setOne(thirdClass);
+					}
+				else if(userClassFrom == 1 && userClassInto == 2)
+					{
+						DataBase.students.get(userChoice - 1).setThree(firstClass);
+						DataBase.students.get(userChoice - 1).setTwo(thirdClass);
+					}
+				else if(userClassFrom == userClassInto)
+					{
+						System.out.println("The students schedule will stay the same.");
+						System.out.println();
+					}
+				
+				System.out.println(DataBase.students.get(userChoice - 1).getFirstName() + " " + DataBase.students.get(userChoice - 1).getLastName() + "'s new schedule is:");
+				System.out.println(DataBase.students.get(userChoice - 1).getOne());
+				System.out.println(DataBase.students.get(userChoice - 1).getTwo());
+				System.out.println(DataBase.students.get(userChoice - 1).getThree());
+				System.out.println();
 				
 				
 			}
